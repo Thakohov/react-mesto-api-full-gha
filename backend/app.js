a4056d52beab1135ff785fda47db85155f936a7c
Эльдар <ethakohov@yandex.ru>
2024-01-01 13:58:33 +0300

    Убрал логи

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
 const { errors } = require('celebrate');
-const { logger } = require('express-winston');
+// const { logger } = require('express-winston');
 const handleError = require('./middlewares/HandleError');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+// const { requestLogger, errorLogger } = require('./middlewares/logger');
 const checkCors = require('./middlewares/allowedCors');
 const router = require('./routes');
 
@@ -20,19 +20,19 @@ mongoose.connect(DB_URL);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(logger);
+// app.use(logger);
 
 app.use(checkCors);
-app.use(requestLogger);
+// app.use(requestLogger);
 app.use(router);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 
-app.use(errorLogger);
+// app.use(errorLogger);
 app.use(errors());
 app.use(handleError);
 
